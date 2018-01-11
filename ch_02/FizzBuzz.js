e938976155bec3146b9d01ef0983c8a722a4b4a1
John Smith M <dlink4096@gmail.com>
2018-01-11 21:44:19 +0400

    Add text for exercises.

--- ch_02/FizzBuzz.js
+++ ch_02/FizzBuzz.js
@@ -1,15 +1,24 @@
+/*
+Write a program that uses console.log t
+o print all the numbers from 1 to 100, with two exceptions. 
+For numbers divisible by 3, print "Fizz" instead of the number, 
+and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz", 
+for numbers that are divisible by both 3 and 5 
+(and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+*/
 var i;
 for(i=1; i<=100; i++)
 {
-	if((i % 3 == 0) && (i % 5 == 0))
+	if((i % 3 == 0) && (i % 5 == 0))	// Check for divisibility by 3 and 5
 	{
 		console.log('FizzBuzz');
 	}
-	else if(i % 3 == 0)
+	else if(i % 3 == 0)					// Check for divisibility by 3
 	{
 		console.log("Fizz");
 	}
-	else if(i % 5 == 0)
+	else if(i % 5 == 0)					// Check for divisibility by 5
 	{
 		console.log("Buzz");
 	}
--- ch_02/looping_a_triangle.js
+++ ch_02/looping_a_triangle.js
@@ -1,4 +1,14 @@
-var x='x';
+/*
+Write a loop that makes seven calls to console.log to output the following triangle:
+#
+##
+### 
+#### 
+##### 
+###### 
+#######
+*/
+var x='#';		// Construction block for the triangle
 var str='';
 for(var i=1; i<=7; i++)
 {
